75befce0cb7bad0328b26e63ccd84a823fe805dc
Jules Didry <didryjules@gmail.com>
2023-09-21 10:51:46 +0200

    A

---
+++ Terminale/POO/3.1.py
@@ -0,0 +1,34 @@
+class CompteBancaire :
+    solde = 0
+    def __init__(self,nom,solde) :
+        self.nom = nom
+        self.solde = solde
+
+    def get_nom(self) :
+        return self.nom
+    def get_solde(self) :
+        return self.solde
+    def __str__(self) : 
+        return f"{self.nom,self.solde}"
+    def set_solde(self, sold) :
+        self.solde = sold
+        return self.solde
+    def depot(self,sold) :
+        self.solde += sold
+        return self.solde
+    def retrait(self,sold) :
+        self.solde -= sold
+        if self.solde<sold :
+            print("ATTENTION votre solde est insuffisant pour pouvoir faire ce retrait")
+        else :
+            return self.solde
+    def affichage(self) :
+        print("Le compte bancaire appartient à :", self.get_nom())
+        print("Le solde est de :", self.get_solde(),"$")
+
+Compte2547 = CompteBancaire("Bob",300.1)
+Compte2547.affichage()
+
+     
+
+    

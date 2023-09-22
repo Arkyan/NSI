e2c10987b63bbffe9021fb46d872cdd9605d682f
Jules Didry <didryjules@gmail.com>
2023-09-22 13:51:01 +0200

    Update 3.2.py

--- Terminale/POO/3.2.py
+++ Terminale/POO/3.2.py
@@ -4,5 +4,21 @@ class Table :
         self.matière = matière
         self.masse = masse
         self.longueur = longueur
-        self.largeur
+        self.largeur = largeur
+        self.hauteur = hauteur
+        self.prixvente = prixvente
+        self.prixfab = prixfab
+        self.stock = stock 
+    def get_ref(self) : 
+        return self.ref
+    def get_matiere(self) : 
+        return self.matière
+    def get_masse(self) : 
+        return self.masse
+    def get_longueur(self) : 
+        return self.longueur
+    
+
+
+        
         

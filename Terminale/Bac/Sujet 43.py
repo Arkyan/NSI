c34dac39f133b8ebd6f92822b445d85d8d0f58ff
Jules Didry <didryjules@gmail.com>
2023-09-22 14:28:53 +0200

    df

--- Terminale/Bac/Sujet 43.py
+++ Terminale/Bac/Sujet 43.py
@@ -7,6 +7,13 @@ def ecriture_binaire_entier_positif(n):
         n = n // 2
     tab.reverse()
     return tab
+
+print(ecriture_binaire_entier_positif(0))
+print(ecriture_binaire_entier_positif(2))
+print(ecriture_binaire_entier_positif(105))
+
+print("--------------------------------------------------")
+
         
 #Exo 2
 def tri_bulles(T):
@@ -18,3 +25,8 @@ def tri_bulles(T):
                 T[j] = T[j+1]
                 T[j+1] = temp
     return T
+
+print(tri_bulles([]))
+print(tri_bulles([7]))
+print(tri_bulles([9, 3, 7, 2, 3, 1, 6]))
+print(tri_bulles([9, 7, 4, 3]))
--- Terminale/POO/3.2.py
+++ Terminale/POO/3.2.py
@@ -17,6 +17,46 @@ class Table :
         return self.masse
     def get_longueur(self) : 
         return self.longueur
+    def get_largeur(self) : 
+        return self.largeur
+    def get_hauteur(self) :
+        return self.hauteur
+    def get_prixvente(self) :
+        return self.prixvente
+    def get_prixfab(self) :
+        return self.prixfab
+    def get_stock(self) :
+        return self.stock
+    
+    def set_ref(self, reff) : 
+        self.ref = reff
+        return self.ref
+    def set_matiere(self, matt) : 
+        self.matière = matt
+        return self.matière
+    def set_masse(self, mass) : 
+        self.masse = mass
+        return self.masse
+    def set_longueur(self, longg) : 
+        self.longueur = longg
+        return self.longueur
+    def set_largeur(self, largg) : 
+        self.largeur = largg
+        return self.largeur
+    def set_hauteur(self, hautt) :
+        self.hauteur = hautt
+        return self.hauteur
+    def set_prixvente(self,prixv) :
+        self.prixvente = prixv
+        return self.prixvente
+    def set_prixfab(self,prixf) :
+        self.prixfab = prixf
+        return self.prixfab
+    def set_stock(self, stook) :
+        self.stock = stook
+        return self.stock
+    def AFFICHAGE(self) :
+        print("")
     
 
 

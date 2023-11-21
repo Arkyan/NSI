2d8141b7a17fc660082b8cc8ac772a192e2baaa9
Jules Didry <didryjules@gmail.com>
2023-11-21 10:10:34 +0100

    zrht

--- Terminale/Bac/Sujet 39.py
+++ Terminale/Bac/Sujet 39.py
@@ -1,17 +1,18 @@
 #Exo 1 
 def fibonacci(n):
-    tab = [None]*(n+1)
-    tab[1] = 1
-    tab[2] = 1
-    for i in range(3, n+1):
-        tab[i] = tab[i-1] + tab[i-2]
-    return tab[n]
+    suite = [0] * (n+1)
+    for i in range(1, n+1):
+        if i < 3:
+            suite[i] = 1
+        else:
+            suite[i] = suite[i-1] + suite[i-2]
+    return suite[n]
 
-#print(fibonacci(1))
+print(fibonacci(1))
 print(fibonacci(2))
 print(fibonacci(25))
 print(fibonacci(45))
-#assert fibonacci(1) == 1
+assert fibonacci(1) == 1
 assert fibonacci(2) == 1
 assert fibonacci(25) == 75025
 assert fibonacci(45) == 1134903170
@@ -37,6 +38,6 @@ notes_nsi = [30, 40, 80, 60, 58, 80, 75, 80, 60, 24]
 
 print(pantheon(eleves_nsi, notes_nsi)) 
 assert pantheon(eleves_nsi, notes_nsi) == (80, ['c', 'f', 'h'])
-print(pantheon([],[]))
+print(pantheon([],[])) 
 assert pantheon([],[]) == (0, [])
     

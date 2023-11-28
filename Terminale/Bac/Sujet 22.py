b657185804b756476f02bbd2ba2537e04a2e46d4
Jules Didry <didryjules@gmail.com>
2023-11-28 11:54:28 +0100

    jrynwf,g

--- Terminale/Bac/Sujet 22.py
+++ Terminale/Bac/Sujet 22.py
@@ -30,7 +30,7 @@ assert liste_puissances_borne(2, 16) == [2, 4, 8]
 assert liste_puissances_borne(2, 17) == [2, 4, 8, 16]
 assert liste_puissances_borne(5, 5) == []
             
-    
+print("------------------------------------------------")   
 #Exo 2
 dico = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6,
         "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12,

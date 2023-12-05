323d1614220f83ade0ec5e17e98a2b482f631857
Jules Didry <didryjules@gmail.com>
2023-12-05 10:13:22 +0100

    fazafz

--- Terminale/Bac/Sujet 2.py
+++ Terminale/Bac/Sujet 2.py
@@ -10,6 +10,12 @@ def indices_maxi(tab):
             t_maxi.append(i)
     return maxi, t_maxi
 
+print(indices_maxi([1, 5, 6, 9, 1, 2, 3, 7, 9, 8]))
+print(indices_maxi([7]))
+assert indices_maxi([1, 5, 6, 9, 1, 2, 3, 7, 9, 8]) == (9, [3, 8])
+assert indices_maxi([7]) == (7, [0])
+
+
 def positif(pile):
     pile_1 = list(pile)
     pile_2 = []
@@ -21,3 +27,8 @@ def positif(pile):
         x = pile_2.pop()
         pile_1.append(x)
     return pile_1
+
+print(positif([-1, 0, 5, -3, 4, -6, 10, 9, -8]))
+print(positif([-2])) 
+assert positif([-1, 0, 5, -3, 4, -6, 10, 9, -8]) == [0, 5, 4, 10, 9]
+assert positif([-2]) == []

99ad2d875b6295e13d97b364ea898c70c9e5cc2f
Jules Didry <didryjules@gmail.com>
2023-12-22 15:09:55 +0100

    tyé

--- Terminale/Bac/Sujet 7.py
+++ Terminale/Bac/Sujet 7.py
@@ -23,7 +23,11 @@ def fusion(tab1, tab2):
 print(fusion([3, 5], [2, 5]))
 print(fusion([-2, 4], [-3, 5, 10]))
 print(fusion([4], [2, 6]))
+assert fusion([3, 5], [2, 5]) == [2, 3, 5, 5]
+assert fusion([-2, 4], [-3, 5, 10]) == [-3, -2, 4, 5, 10]
+assert fusion([4], [2, 6]) == [2, 4, 6]
 
+print("---------------------")
 
 #Exo 2
 romains = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
@@ -38,5 +42,9 @@ def traduire_romain(nombre) :
         return  traduire_romain(nombre[1:]) - romains[nombre[0]]
 
 print(traduire_romain("XIV"))
-print(traduire("CXLII"))
+print(traduire_romain("CXLII"))
 print(traduire_romain("MMXXIII"))
+assert traduire_romain("XIV") == 14
+assert traduire_romain("CXLII") == 142
+assert traduire_romain("MMXXIII") == 2023
+

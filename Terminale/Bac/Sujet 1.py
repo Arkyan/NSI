a803b8251d250892a27a4dede1dee799acbbab4c
Arkyan <didryjules@gmail.com>
2024-01-11 13:40:01 +0100

    fz

--- Terminale/Bac/Sujet 1.py
+++ Terminale/Bac/Sujet 1.py
@@ -20,6 +20,7 @@ assert verifie([-1, 4]) == True
 assert verifie([5]) == True
 
 
+print("----------------------------------")
 
 #Exo 2
 urne = ['A', 'A', 'A','B', 'C', 'B', 'C','B', 'C', 'B']
@@ -34,12 +35,10 @@ def depouille(urne):
     return resultat
 
 def vainqueur(election):
-    vainqueur = '' #cette variable ne sert Ã  rien
     nmax = 0
     for candidat in election:
         if election[candidat] > nmax :
             nmax = election[candidat]
-            vainqueur = candidat #cette ligne ne sert Ã  rien !
     liste_finale = [nom for nom in election if election[nom] == nmax]
     return liste_finale
 

79ec20b2e1b540cab3a41fc56c6b5bb550090dc9
Jules Didry <didryjules@gmail.com>
2024-01-18 10:05:09 +0100

    fafffazfa

--- Terminale/Bac/Sujet 5.py
+++ Terminale/Bac/Sujet 5.py
@@ -15,6 +15,25 @@ def paire_6(tab) :
             n = n + 1
     return n >= 2
 
+lancer1 = lancer(5)
+print(paire_6(lancer1))
+assert paire_6([1, 2, 3, 4, 5]) == True
+
+lancer2 = lancer(5)
+print(paire_6(lancer2))
+assert paire_6([1, 2, 3, 4, 6]) == True
+
+lancer3 = lancer(3)
+print(paire_6(lancer3))
+assert paire_6([1, 6, 6]) == False
+
+lancer4 = lancer(0)
+print(paire_6(lancer4))
+assert paire_6([]) == False
+
+
+
+print("-----------------")
 #Exo 2
 img=[[20, 34, 254, 145, 6], [23, 124, 237, 225, 69], [197, 174, 207, 25, 87], [255, 0, 24, 197, 189]]
 
@@ -43,4 +62,14 @@ def binaire(image, seuil):
             else:
                 L[i][j] = 1
     return L
+
+nbLig(img)
+assert nbLig(img) == 4
+nbCol(img)
+assert nbCol(img) == 5
+negatif(img)
+assert negatif(img) == [[235, 221, 1, 110, 249], [232, 131, 18, 30, 186], [58, 81, 48, 230, 168], [0, 255, 231, 58, 66]]
+binaire(img,120)
+assert binaire(img,120) == [[0, 0, 1, 1, 0], [0, 1, 1, 1, 0], [1, 1, 1, 0, 0], [1, 0, 1, 1, 1]]
+
 

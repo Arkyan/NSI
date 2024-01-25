e4e1e0018088ce2e45cefff4d1ca5f11edad4515
Jules Didry <didryjules@gmail.com>
2024-01-25 11:52:54 +0100

    dezfe

--- Terminale/Bac/Sujet 40.py
+++ Terminale/Bac/Sujet 40.py
@@ -10,7 +10,18 @@ def nombre_de_mots(phrase):
     else :
         return nb+1
 
+print('nombre_de_mots("Cet exercice est simple.") =', nombre_de_mots("Cet exercice est simple."))
+print('nombre_de_mots("Le point d exclamation est separe !") =', nombre_de_mots("Le point d exclamation est separe !"))
+print('nombre_de_mots("Combien de mots y a t il dans cette phrase ?") =', nombre_de_mots("Combien de mots y a t il dans cette phrase ?"))
+print('nombre_de_mots("Fin.") =', nombre_de_mots("Fin."))
 
+assert nombre_de_mots("Cet exercice est simple.") == 4
+assert nombre_de_mots("Le point d exclamation est separe !") == 6
+assert nombre_de_mots("Combien de mots y a t il dans cette phrase ?") == 10
+assert nombre_de_mots("Fin.") == 1
+
+
+print('-------------------')
 
 #Exo 2
 class Noeud:
@@ -35,3 +46,17 @@ class Noeud:
                 self.droit.inserer(cle)
             else:
                 self.droit  = Noeud(cle)
+
+arbre = Noeud(7)
+for cle in (3, 9, 1, 6):
+    arbre.inserer(cle)
+    
+print("arbre.gauche.getValeur() =", arbre.gauche.getValeur())
+print("arbre.droit.getValeur() =", arbre.droit.getValeur())
+print("arbre.gauche.gauche.getValeur() =", arbre.gauche.gauche.getValeur())
+print("arbre.gauche.droit.getValeur() =", arbre.gauche.droit.getValeur())
+
+assert arbre.gauche.getValeur() == 3
+assert arbre.droit.getValeur() == 9
+assert arbre.gauche.gauche.getValeur() == 1
+assert arbre.gauche.droit.getValeur() == 6

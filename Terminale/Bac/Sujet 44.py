5efae09357455e66f727bb8d674a2710c61180b9
Jules Didry <didryjules@gmail.com>
2024-03-19 10:06:57 +0100

    afce

--- Terminale/Bac/Sujet 44.py
+++ Terminale/Bac/Sujet 44.py
@@ -1,40 +1,25 @@
 #Exo 1
-def enumere(tab):
-    d = {}
-    for i in range(len(tab)):
-        if tab[i] not in d:
-            d[tab[i]] = [i]
-        else :
-            d[tab[i]].append(i)
-    return d
+def renverse(mot):
+    renv = ''
+    for c in mot:
+        renv = c + renv
+    return renv
+
+print("renverse('informatique') =", renverse('informatique'))
+assert renverse('informatique') == 'euqitamrofni'
 
 
 #Exo 2
-class Noeud:
-    def __init__(self, etiquette, gauche, droit):
-        self.etiquette = etiquette
-        self.gauche = gauche
-        self.droit = droit
+def crible(n):
+    premiers = []
+    tab = [True] * n
+    tab[0], tab[1] = False, False
+    for i in range(2, n):
+        if tab[i] == True:
+            premiers.append(i)
+            for multiple in range(2 * i, n, i):
+                tab[multiple] = False
+    return premiers
 
-def parcours(arbre, liste):
-    if arbre != None:
-        parcours(arbre.gauche, liste)
-        liste.append(arbre.etiquette)
-        parcours(arbre.droit, liste)
-    return liste
-
-def insere(arbre, cle):
-    if arbre == None:
-        return Noeud(cle, None, None) # creation d'une feuille
-    else:
-        if cle < arbre.etiquette: 
-            arbre.gauche = insere(arbre.gauche, cle)
-        else:
-            arbre.droit = insere(arbre.droit, cle) 
-        return arbre
-
-
-a_5 = Noeud(5,None, None)
-a_2 = insere(a_5,2)
-a_7 = insere(a_2,7)
-a_3 = insere(a_7,3)
+print("crible(40) =", crible(40))
+assert crible(40) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]

c940fa09bc38e2a00f23a45631fc5413c8f4f6e5
Jules Didry <didryjules@gmail.com>
2024-03-19 10:50:53 +0100

    gvbrz

--- Terminale/Bac/Sujet 45.py
+++ Terminale/Bac/Sujet 45.py
@@ -1,9 +1,4 @@
-"""
-Exercice N°1
-"""
-
-print("\033[92mExercice N°1\033[0m")
-
+#Exo 1
 def rangement_valeurs(notes_eval):
     valeurs_final = []
     for i in range(11):
@@ -23,20 +18,15 @@ def notes_triees(effectifs):
 
 notes_eval = [2, 0, 5, 9, 6, 9, 10, 5, 7, 9, 9, 5, 0, 9, 6, 5, 4]
 
-print("rangement_valeurs(notes_eval) ->", rangement_valeurs(notes_eval))
-print("notes_triees(rangement_valeurs(notes_eval)) ->", notes_triees(rangement_valeurs(notes_eval)))
+print("rangement_valeurs(notes_eval) =", rangement_valeurs(notes_eval))
+print("notes_triees(rangement_valeurs(notes_eval)) =", notes_triees(rangement_valeurs(notes_eval)))
 
 assert rangement_valeurs(notes_eval) == [2, 0, 1, 0, 1, 4, 2, 1, 0, 5, 1]
 assert notes_triees(rangement_valeurs(notes_eval)) == [0, 0, 2, 4, 5, 5, 5, 5, 6, 6, 7, 9, 9, 9, 9, 9, 10]
 
-print()
+print("--------------------")
 
-"""
-Exercice N°2
-"""
-
-print("\033[92mExercice N°2\033[0m")
-
+#Exo 2
 def dec_to_bin (nb_dec):
     q, r = nb_dec // 2, nb_dec % 2
     if q == 0:
@@ -56,10 +46,10 @@ def bin_to_dec(nb_bin):
             bit_droit = 1
         return 2 * bin_to_dec(nb_bin[:-1]) + bit_droit
 
-print("dec_to_bin(25) ->", dec_to_bin(25))
+print("dec_to_bin(25) =", dec_to_bin(25))
 
 assert dec_to_bin(25) == "11001"
 
-print("bin_to_dec('101010') ->", bin_to_dec('101010'))
+print("bin_to_dec('101010') =", bin_to_dec('101010'))
 
 assert bin_to_dec('101010') == 42
